3af9240dc8fe35fff9a90e1ec0ce913f382f2011
FSDAbhinandan <bajpai.abhi326@gmail.com>
2023-05-07 02:47:18 +0530

    R3 Commit Message2

---
+++ src/main/java/com/example/newTestGitProject/controller/TestRel4Controller.java
@@ -0,0 +1,14 @@
+package com.example.newTestGitProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRel4Controller {
+	@GetMapping("/name")
+	public void printName() {
+		System.out.println("NAME");
+	} 
+	
+
+}
