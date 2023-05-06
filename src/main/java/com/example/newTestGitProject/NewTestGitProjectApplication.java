f962a7b4169268e9adaa82eb44e955769ee5e1bb
FSDAbhinandan <bajpai.abhi326@gmail.com>
2023-05-07 02:08:57 +0530

    message 1

---
+++ src/main/java/com/example/newTestGitProject/NewTestGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.newTestGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTestGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTestGitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newTestGitProject/NewTestGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newTestGitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTestGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
