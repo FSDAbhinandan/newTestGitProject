6ffe2c8c0dfac53a27d5f268b833f9df199f6add
FSDAbhinandan <bajpai.abhi326@gmail.com>
2023-05-07 02:27:41 +0530

    Rel1

---
+++ src/main/java/com/example/newTestGitProject/controller/TestRel1.java
@@ -0,0 +1,13 @@
+package com.example.newTestGitProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRel1 {
+
+	@GetMapping("/name")
+	public void printName() {
+		System.out.println("NAME");
+	} 
+}
